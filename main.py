6b909935fe73088ec9a82f55fc1276b292ab6676
lisi <lisi@qq.com>
2025-01-20 14:00:12 +0800

    collision

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame
 
 # 限制物体在屏幕内
@@ -20,11 +22,30 @@ pygame.display.set_caption("Pytmx Demo")
 
 player_x = 50
 player_y = 50
-player_speed = 0.3  # 玩家速度
+player_speed = 3  # 玩家速度
 
 
 # 加载 .tmx 地图文件
 tmx_data = pytmx.load_pygame("demo2.tmx")  # 将 'your_map.tmx' 替换为你的文件路径
+
+# 检查某个位置是否有墙体
+def check_collision(x, y):
+    # 获取该位置的瓦片 ID
+    dic = None
+    try:
+        x_pos = math.floor(x / tmx_data.tilewidth)
+        y_pos = math.floor(y / tmx_data.tileheight)
+        # print(x, y)
+        print(x_pos, y_pos)
+        dic = tmx_data.get_tile_properties(x_pos, y_pos, layer=1)
+    except Exception as e:
+        pass
+
+    # print(gid)
+    if dic:
+        return dic['Collidable'] == True
+    else:
+        return False
 
 # 创建一个地图渲染函数
 def draw_map():
@@ -39,6 +60,7 @@ def draw_map():
 # 游戏主循环
 running = True
 while running:
+    clock = pygame.time.Clock()
     draw_map()  # 绘制地图
 
     for event in pygame.event.get():
@@ -51,23 +73,29 @@ while running:
 
     if keys[pygame.K_LEFT]:
         new_x -= player_speed  # 向左移动
-        player_x = new_x
+        if not check_collision(new_x, new_y):
+            player_x = new_x
 
     if keys[pygame.K_RIGHT]:
         new_x += player_speed  # 向右移动
-        player_x = new_x
+        if not check_collision(new_x, new_y):
+            player_x = new_x
 
     if keys[pygame.K_UP]:
         new_y -= player_speed  # 向上移动
-        player_y = new_y
+        if not check_collision(new_x, new_y):
+            player_y = new_y
 
     if keys[pygame.K_DOWN]:
         new_y += player_speed  # 向下移动
-        player_y = new_y
+        if not check_collision(new_x, new_y):
+            player_y = new_y
 
     # 限制玩家位置在屏幕内
     player_x, player_y = limit_position_to_screen(player_x, player_y, 32, 32)
     pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(player_x, player_y, 32, 32))  # 32x32 是玩家的大小
     pygame.display.flip()  # 更新显示
+    # 控制帧率
+    clock.tick(30)
 
 pygame.quit()
